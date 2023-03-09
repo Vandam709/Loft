4f127864f9979937ee65d2133c7beb20228cedce
wsrvs@outlook.com <wsrvs@outlook.com>
2023-03-09 13:50:14 +0300

    Добавьте файлы проекта.

---
+++ AppData/Helpers/ClassConnect.cs
@@ -0,0 +1,7 @@
+﻿namespace ShopMag.AppData.Helpers
+{
+    class ClassConnect
+    {
+        public static Models.user21Entities user21Entities = new Models.user21Entities();
+    }
+}
---
+++ Pages/OknoProfile.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ShopMag.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для OknoProfile.xaml
+    /// </summary>
+    public partial class OknoProfile : Window
+    {
+        public OknoProfile()
+        {
+            InitializeComponent();
+            LViewSklad.ItemsSource = AppData.Helpers.ClassConnect.user21Entities.Reservations.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
